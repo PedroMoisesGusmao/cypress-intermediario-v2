f17f72849d2ef98ab2a1b62bd339a6e1b5eb0a20
Pedro <pedroaraujo4910@gmail.com>
2023-12-07 10:36:03 -0300

    Aula2 ex0

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,20 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Teste GitLab', () => {
+  it('realiza login', () => {
+    cy.visit("localhost")
+    
+    cy.get("#user_login")
+    .type("pedroaraujo4910@gmail.com")
+    .should("have.value", "pedroaraujo4910@gmail.com")
+
+    cy.get("#user_password")
+    .type("12345678")
+    .should("have.value", "12345678")
+
+    cy.get('input[type="submit"]')
+    .first()
+    .click()
+
+    cy.get(".blank-state-welcome-title")
+    .should("be.visible")
   })
 })
