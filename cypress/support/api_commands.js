a954a5010444fab239937e58a5e6f059c278ac0e
Pedro <pedroaraujo4910@gmail.com>
2023-12-14 10:35:52 -0300

    Aula5 ex2

--- cypress/support/api_commands.js
+++ cypress/support/api_commands.js
@@ -6,11 +6,31 @@ Cypress.Commands.add('api_createProject', project => {
   cy.request({
     method: 'POST',
     url: `/api/v4/projects/`,
-    headers: { Authorization: accessToken },
     body: {
       name: project.name,
       description: project.description,
       initialize_with_readme: true
-    }
+    },
+    headers: { Authorization: accessToken },
   })
+})
+
+Cypress.Commands.add('api_getAllProjects', () => {
+  cy.request({
+    method: 'GET',
+    url: `/api/v4/projects/`,
+    headers: { Authorization: accessToken },
+  })
+})
+
+Cypress.Commands.add('api_deleteProjects', projectId => {
+  cy.api_getAllProjects()
+     .then(res => {
+      res.body.forEach(project => cy.request({
+          method: 'DELETE',
+          url: `/api/v4/projects/${project.id}`,
+          headers: { Authorization: accessToken}
+      })
+    )
+    })
 })
